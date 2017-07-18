b7a799b49ce3dcb3cd1e9151a85f1c3799effe72
niminjie <niminjie@baidu.com>
2017-07-18 21:39:22 +0800

    update

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -8,43 +8,77 @@ export default {
 
     data() {
         return {
+
+            // page load or not
             loaded: false,
+
+            // current subscription id
+            curSub: '',
+
+            // subscriptions list
             subscriptions: [],
+
+            // current subscription's feeds
             curItems: [],
+
+            // Page size
             pageSize: 10,
+
+            // Current page no
             pageNo: 1,
+
+            // scroll height
+            scrollHeight: 0,
         }
     },
 
     computed: {
+
+        // sort publish time desc
         sortedItems() {
             return  _.orderBy(this.curItems, ['timestampUsec'], ['desc'])
                 .filter(item => item && item.canonical && item.summary)
         },
 
+        // pagniation items
         displayItems() {
             return this.sortedItems.slice(0, this.pageNo * this.pageSize)
         }
     },
 
     async mounted() {
+
+        // Create feed database
         this.feed = new Feeds('subscriptions')
 
+        // Fetch subscriptions list
         const subscriptions = await this.feed.fetchSubscriptions()
         this.loaded = true
         this.subscriptions = subscriptions
 
-        this.intervalSync()
-
+        // Observe pagination
         setTimeout(() => {
             this.io = new IntersectionObserver(this.nextPage);
             this.io.observe(this.$refs.footer)
         }, 0)
+
+        // Auto sync feeds
+        this.intervalSync()
     },
 
     methods: {
-        intervalSync() {
-            this.feed.fetchFeeds(this.subscriptions)
+        async intervalSync() {
+            await this.feed.fetchFeeds(this.subscriptions)
+
+            if (this.curSub) {
+                if (this.curSub === 'all') {
+                    this.showAll()
+                }
+                else {
+                    this.checkSub({ id: this.curSub })
+                }
+            }
+
             setTimeout(() => {
                 this.intervalSync()
             }, 1000 * 60 * 5)
@@ -65,8 +99,8 @@ export default {
                 const items = await this.feed.feedsBySub(id)
 
                 if (items && items.docs && items.docs.length) {
-                    console.log('Check items', items.docs);
                     this.curItems = items.docs
+                    this.curSub = id
                 }
 
                 this.pageNo = 1
@@ -82,6 +116,7 @@ export default {
 
                 if (items && items.rows && items.rows.length) {
                     this.curItems = items.rows.map(row => row.doc)
+                    this.curSub = 'all'
                 }
 
                 this.pageNo = 1
